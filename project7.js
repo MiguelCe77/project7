61d47d02bb482146079f6ba6c399f2d7752ff976
Miguel Ceballos <ceballosmiguel777@gmail.com>
2016-12-13 08:58:45 -0800

    its work

--- project7.js
+++ project7.js
@@ -12,11 +12,42 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+  var newrect = document.createElementNS(namespace,"rect");
+  newrect.setAttribute("fill",color);
+  newrect.setAttribute("y",y);
+  newrect.setAttribute("x",x);
+  newrect.setAttribute("width",size);
+  newrect.setAttribute("height",size);
+  canvas.appendChild(newrect);
+}
+
+function drawCircle(x, y, size, color) {
+  var newcircle = document.createElementNS(namespace,"circle");
+  newcircle.setAttribute("fill",color);
+  newcircle.setAttribute("cy",y);
+  newcircle.setAttribute("cx",x);
+  newcircle.setAttribute("r",size);
+  canvas.appendChild(newcircle);
 }
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
   // what do you want to do when the user presses down
   // on the mouse button?
+})
+
+document.addEventListener("mouseup", function(e) {
+
+})
+
+document.addEventListener("mousemove", function(e) {
+    var pt = transformPoint(e, screen)
+    var xpos = pt.x
+    var ypos = pt.y
+    var newcircle = document.createElementNS(namespace,"circle")
+    newcircle.setAttribute("cx", pt.x)
+    newcircle.setAttribute("cy", pt.y)
+    newcircle.setAttribute("fill", document.getElementById("colorSelect").value)
+    newcircle.setAttribute("r", "25")
+    screen.appendChild(newcircle)
 })
